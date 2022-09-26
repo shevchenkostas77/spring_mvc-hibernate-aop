da5db391adef0fe2a17bf4891e042f878525c66b
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-09-26 12:14:50 +0300

    Show list of all workers

---
+++ src/main/java/com/shevchenkostas77/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -0,0 +1,33 @@
+package com.shevchenkostas77.spring.mvc_hibernate_aop.dao;
+
+import com.shevchenkostas77.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional // спринг автоматически управляет открытием и закрытием транзакций
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+
+//        List<Employee> allEmployees = session.createQuery("from Employee"
+//                , Employee.class).getResultList();
+
+        Query<Employee> query = session.createQuery("from Employee"
+                , Employee.class);
+        List<Employee> allEmplyees = query.getResultList();
+
+        return allEmplyees;
+    }
+}
